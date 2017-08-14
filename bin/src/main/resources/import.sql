69aa2a557b4a776a52291ae3eb1564f792841e5a
Collin <Collin@Collin-PC>
2017-08-14 22:47:49 +0200

    Löschung der Bin

--- bin/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE DROHNE ( ID INT, LIEFERORT VARCHAR(50), GEWICHT VARCHAR(5));
-
-INSERT INTO DROHNE VALUES ( 1, 'Kingston', '3' ), ( 2, 'Budapest', '2' ), ( 3, 'Krefeld', '2.5' ), ( 4, 'Deutschland', '0.5' ), ( 5, 'Stadt', '4' );
