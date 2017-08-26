409ce89f3711e7ffd839dbd4bc55444198680667
Darlith <Owner@10.2.120.222>
2017-08-26 13:52:48 +0200

    Umsetzung Backend erster Teil (sollte funktional und fehlerfrei sein)

--- src/main/java/com/eufh/drohne/domain/Drohne.java
+++ src/main/java/com/eufh/drohne/domain/Drohne.java
@@ -1,9 +1,8 @@
 package com.eufh.drohne.domain;
 
-import java.sql.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,15 +11,22 @@ import javax.persistence.Table;
 public class Drohne {
 
 	@Id
+	@GeneratedValue
 	@Column(name = "ID")
 	private int id;
 	@Column(name = "LIEFERORT")
 	private String lieferort;
 	@Column(name = "GEWICHT")
 	private String gewicht;
+	private double totalPackageWeight; //kg
+	private int packageCount;
+	private double totalDistance; //km
+	private final int speed = 60; // kmh
 	
 	public Drohne() {
-		
+		this.totalPackageWeight = 0.0;
+		this.packageCount = 0;	
+		this.setTotalDistance(0.0);
 	}
 	
 	public Drohne(int id, String lieferort, String gewicht){
@@ -46,6 +52,46 @@ public class Drohne {
 	}
 	public void setGewicht(String gewicht) {
 		this.gewicht = gewicht;
+	}
+	/**
+	 * Adds a package to the drone
+	 * 
+	 * @param weight Weight of the new package
+	 */
+	public void addPackage(double weight) {
+			this.packageCount++;
+			this.totalPackageWeight += weight;
+	}
+	
+	public void resetDrone() {
+		this.totalPackageWeight = 0.0;
+		this.packageCount = 0;
+		this.setTotalDistance(0.0);
+	}
+	
+	public Drohne findDroneById(int id)
+	{
+		if(this.id == id)
+		{
+			return this;
+		}
+		return null;
+	}
+
+	public double getTotalDistance() {
+		return totalDistance;
+	}
+
+	public void setTotalDistance(double totalDistance) {
+		this.totalDistance = totalDistance;
+	}
+	
+	public double getTotalPackageWeight() {
+		return totalPackageWeight;
+	}
+	
+	public int getPackageCount() {
+		return packageCount;
 	}
 
 }
